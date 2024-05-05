222b39921e126b1dbd09716cfa3f1de0498d89f9
Hayk Hovhannisyan <hovahnnisayn@mail.ru>
2024-05-05 13:43:40 +0400

    Add console argument handling
    - Implement command-line argument parsing using argparse
    - Allow users to specify URL and directory via command-line arguments
    - Add error handling for invalid URL and directory

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 import os
 from urllib.parse import urlparse, urljoin
+import argparse
+import validators
 
 
 def download_images(url, directory):
@@ -18,12 +20,37 @@ def download_images(url, directory):
             abs_path = os.path.join(directory, img_name)
             with open(abs_path, 'wb') as file:
                 file.write(requests.get(img_url).content)
-    except FileNotFoundError:
-        print("Directory not found.")
     except Exception as e:
         print(f"An error occurred: {e}")
 
 
-url = input("URL: ")
-directory = input("Path to the directory: ")
-download_images(url, directory)
+def main():
+    parser = argparse.ArgumentParser(description='Download images from a webpage.')
+    parser.add_argument('-url', '--url', type=str, help='URL of the webpage containing images')
+    parser.add_argument('-d', '--directory', type=str, help='Directory to save downloaded images')
+    args = parser.parse_args()
+
+
+    url = args.url
+    directory = args.directory
+
+    if url is None or directory is None:
+        print("Error: Please provide both URL and directory arguments.")
+        return
+
+    if not validators.url(url):
+        print("Error: Invalid URL provided.")
+        return
+
+    if os.path.exists(directory):
+        download_images(url, directory)
+    else:
+        try:
+            os.makedirs(directory)
+            download_images(url, directory)
+        except Exception as e:
+            print(f"Error: Failed to create directory - {e}")
+
+
+if __name__ == "__main__":
+    main()
