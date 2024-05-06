a80be90f6108590c6cb2dd953901d26264a8daf8
Hayk2508 <145898819+Hayk2508@users.noreply.github.com>
2024-05-06 21:19:06 +0400

    Add console argument handling (#3)
    * Add console argument handling
    
    - Implement command-line argument parsing using argparse
    - Allow users to specify URL and directory via command-line arguments
    - Add error handling for invalid URL and directory
    
    
    * Moved downloading one image to separate function,removed try/except,added keywords arguments

--- main.py
+++ main.py
@@ -2,28 +2,59 @@ import requests
 from bs4 import BeautifulSoup
 import os
 from urllib.parse import urlparse, urljoin
+import argparse
+import validators
 
 
-def download_images(url, directory):
-    try:
-        html_content = requests.get(url).text
-        soup = BeautifulSoup(html_content, 'lxml')
-        img_tags = soup.find_all('img')
-        for img in img_tags:
-            img_url = img.get('src')
-            if not img_url.startswith(('http://', 'https://')):
-                base_url = '{uri.scheme}://{uri.netloc}'.format(uri=urlparse(url))
-                img_url = urljoin(base_url, img_url)
-            img_name = os.path.basename(img_url)
-            abs_path = os.path.join(directory, img_name)
-            with open(abs_path, 'wb') as file:
-                file.write(requests.get(img_url).content)
-    except FileNotFoundError:
-        print("Directory not found.")
-    except Exception as e:
-        print(f"An error occurred: {e}")
+def download_image(img_url, directory):
+    img_name = os.path.basename(img_url)
+    abs_path = os.path.join(directory, img_name)
+    with open(abs_path, 'wb') as file:
+        file.write(requests.get(img_url).content)
 
 
-url = input("URL: ")
-directory = input("Path to the directory: ")
-download_images(url, directory)
+def download_images(images_url, directory):
+    for img_url in images_url:
+        download_image(img_url=img_url, directory=directory)
+
+
+def parse_images(url):
+    images_url = []
+    html_content = requests.get(url).text
+    soup = BeautifulSoup(html_content, 'lxml')
+    img_tags = soup.find_all('img')
+    for img in img_tags:
+        img_url = img.get('src')
+        if not img_url.startswith(('http://', 'https://')):
+            base_url = '{uri.scheme}://{uri.netloc}'.format(uri=urlparse(url))
+            img_url = urljoin(base_url, img_url)
+        images_url.append(img_url)
+    return images_url
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Download images from a webpage.')
+    parser.add_argument('-url', '--url', type=str, help='URL of the webpage containing images')
+    parser.add_argument('-d', '--directory', type=str, help='Directory to save downloaded images')
+    args = parser.parse_args()
+
+    url = args.url
+    directory = args.directory
+
+    if url is None or directory is None:
+        print("Error: Please provide both URL and directory arguments.")
+        return
+
+    if not validators.url(url):
+        print("Error: Invalid URL provided.")
+        return
+
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    images_url = parse_images(url)
+    download_images(images_url=images_url, directory=directory)
+
+
+if __name__ == "__main__":
+    main()
