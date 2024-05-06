883421a54685bb8ec998c5e919e769a8e1602cb6
Hayk Hovhannisyan <hovahnnisayn@mail.ru>
2024-05-06 19:29:37 +0400

    Moved the logic for downloading images from the main function to a separate function,also simplify directory creation logic,by removing try/except

--- main.py
+++ main.py
@@ -6,16 +6,23 @@ import argparse
 import validators
 
 
-def download_images(url, directory):
+def parse_images(url):
+    images_url = []
+    html_content = requests.get(url).text
+    soup = BeautifulSoup(html_content, 'lxml')
+    img_tags = soup.find_all('img')
+    for img in img_tags:
+        img_url = img.get('src')
+        if not img_url.startswith(('http://', 'https://')):
+            base_url = '{uri.scheme}://{uri.netloc}'.format(uri=urlparse(url))
+            img_url = urljoin(base_url, img_url)
+        images_url.append(img_url)
+    return images_url
+
+
+def download_images(images_url, directory):
     try:
-        html_content = requests.get(url).text
-        soup = BeautifulSoup(html_content, 'lxml')
-        img_tags = soup.find_all('img')
-        for img in img_tags:
-            img_url = img.get('src')
-            if not img_url.startswith(('http://', 'https://')):
-                base_url = '{uri.scheme}://{uri.netloc}'.format(uri=urlparse(url))
-                img_url = urljoin(base_url, img_url)
+        for img_url in images_url:
             img_name = os.path.basename(img_url)
             abs_path = os.path.join(directory, img_name)
             with open(abs_path, 'wb') as file:
@@ -30,7 +37,6 @@ def main():
     parser.add_argument('-d', '--directory', type=str, help='Directory to save downloaded images')
     args = parser.parse_args()
 
-
     url = args.url
     directory = args.directory
 
@@ -42,14 +48,11 @@ def main():
         print("Error: Invalid URL provided.")
         return
 
-    if os.path.exists(directory):
-        download_images(url, directory)
-    else:
-        try:
-            os.makedirs(directory)
-            download_images(url, directory)
-        except Exception as e:
-            print(f"Error: Failed to create directory - {e}")
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    images_url = parse_images(url)
+    download_images(images_url, directory)
 
 
 if __name__ == "__main__":
