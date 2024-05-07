a86dae183b6f6ee0d6e0adedb908bd494a0f0617
Hayk Hovhannisyan <hovahnnisayn@mail.ru>
2024-05-07 17:00:22 +0400

    Added class-based approach

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from abc import ABC, abstractmethod
 import requests
 from bs4 import BeautifulSoup
 import os
@@ -6,30 +7,36 @@ import argparse
 import validators
 
 
-def download_image(img_url, directory):
-    img_name = os.path.basename(img_url)
-    abs_path = os.path.join(directory, img_name)
-    with open(abs_path, 'wb') as file:
-        file.write(requests.get(img_url).content)
+class Parser(ABC):
+    @abstractmethod
+    def parse(self, url: str):
+        pass
 
 
-def download_images(images_url, directory):
-    for img_url in images_url:
-        download_image(img_url=img_url, directory=directory)
+class ImgParser(Parser):
+    def __init__(self):
+        self.image_urls = []
 
+    def parse(self, url: str):
+        html_content = requests.get(url).text
+        soup = BeautifulSoup(html_content, 'lxml')
+        img_tags = soup.find_all('img')
+        for img in img_tags:
+            img_url = img.get('src')
+            if not img_url.startswith(('http://', 'https://')):
+                base_url = '{uri.scheme}://{uri.netloc}'.format(uri=urlparse(url))
+                img_url = urljoin(base_url, img_url)
+            self.image_urls.append(img_url)
 
-def parse_images(url):
-    images_url = []
-    html_content = requests.get(url).text
-    soup = BeautifulSoup(html_content, 'lxml')
-    img_tags = soup.find_all('img')
-    for img in img_tags:
-        img_url = img.get('src')
-        if not img_url.startswith(('http://', 'https://')):
-            base_url = '{uri.scheme}://{uri.netloc}'.format(uri=urlparse(url))
-            img_url = urljoin(base_url, img_url)
-        images_url.append(img_url)
-    return images_url
+    def download_image(self, img_url: str, directory: str):
+        img_name = os.path.basename(img_url)
+        abs_path = os.path.join(directory, img_name)
+        with open(abs_path, 'wb') as file:
+            file.write(requests.get(img_url).content)
+
+    def save_images(self, directory: str):
+        for img_url in self.image_urls:
+            self.download_image(img_url=img_url, directory=directory)
 
 
 def main():
@@ -52,8 +59,10 @@ def main():
     if not os.path.exists(directory):
         os.makedirs(directory)
 
-    images_url = parse_images(url)
-    download_images(images_url=images_url, directory=directory)
+    img_parser = ImgParser()
+    img_parser.parse(url=url)
+    img_parser.save_images(directory=directory)
+    print("Images downloaded successfully.")
 
 
 if __name__ == "__main__":
