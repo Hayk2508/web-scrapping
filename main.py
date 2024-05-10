24eaca6f082f09f8cd625b3d5ad58769329fffbf
Hayk Hovhannisyan <hovahnnisayn@mail.ru>
2024-05-10 14:57:50 +0400

    Added new class named video-parser,provided choice to download videos or images

--- main.py
+++ main.py
@@ -7,14 +7,17 @@ import argparse
 import validators
 from parsers.parser_abc import Parser
 from parsers.image_parser import ImgParser
+from parsers.video_parser import VideoParser
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Download images from a webpage.')
-    parser.add_argument('-url', '--url', type=str, help='URL of the webpage containing images')
-    parser.add_argument('-d', '--directory', type=str, help='Directory to save downloaded images')
+    parser = argparse.ArgumentParser(description='Download images/videos from a webpage.')
+    parser.add_argument('-url', '--url', type=str, help='URL of the webpage containing images/videos')
+    parser.add_argument('-d', '--directory', type=str, help='Directory to save downloaded images/videos')
+    parser.add_argument('-v', '--videos',  action='store_true', help='If you want to download videos')
+    parser.add_argument('-i', '--images',  action='store_true', help='If you want to download images')
+    parser.add_argument('-l', '--limit', type=int, help='Set Limit to downloading videos')
     args = parser.parse_args()
-
     url = args.url
     directory = args.directory
 
@@ -29,9 +32,24 @@ def main():
     if not os.path.exists(directory):
         os.makedirs(directory)
 
-    img_parser = ImgParser(url=url, directory=directory)
-    img_parser.parse()
-    print("Images downloaded successfully.")
+    if args.images:
+        img_parser = ImgParser(url=url, directory=directory)
+        img_parser.parse()
+        print("Images downloaded successfully.")
+
+    elif args.videos:
+        if args.limit is None:
+            print("Error: Please provide the limit ")
+            os.removedirs(directory)
+            return
+        max_videos = args.limit
+        video_parser = VideoParser(url=url, directory=directory, max_videos=max_videos)
+        video_parser.parse()
+        print("Videos downloaded successfully.")
+    else:
+        print("Error: Please choose what you want to download:-v/--videos | -i/--images")
+        os.removedirs(directory)
+        return
 
 
 if __name__ == "__main__":
