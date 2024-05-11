631beded2edfe11f35b7cc443018f780826d4bd6
Hayk2508 <145898819+Hayk2508@users.noreply.github.com>
2024-05-11 20:21:28 +0400

    Video-Parser (#5)
    * Added  a new class named video-parser,provided choice to download videos or images
    
    * Added a new abstract class 'MediaParser' that inherits from 'Parser'. Implemented the 'download' method and 'process_url' method to prevent code duplication.
    
    * Added new method 'fetch' in MediaParser to prevent code duplication
    
    * Added pycache to .gitignore
    
    * Update parsers/media_parser.py
    
    Co-authored-by: harutmarg <144890765+harutmarg@users.noreply.github.com>
    
    * Minor changes
    
    * Delete parsers/__pycache__ directory
    
    ---------

---
+++ parsers/video_parser.py
@@ -0,0 +1,23 @@
+from .media_parser import MediaParser
+
+
+class VideoParser(MediaParser):
+    def __init__(self, url: str, directory: str, max_videos):
+        super().__init__(url=url, directory=directory)
+        self.max_videos = max_videos
+
+    def parse(self):
+        video_tags = self.fetch(tag='video')
+        if len(video_tags) > self.max_videos:
+            video_tags = video_tags[:self.max_videos]
+        for video in video_tags:
+            video_url = video.find("a")['href']
+            if not video_url.startswith(('http://', 'https://')):
+                video_url = self.process_url(media_url=video_url)
+            self.download(media_url=video_url)
+
+
+
+
+
+
