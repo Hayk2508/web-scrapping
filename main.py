2ae5463edeee0f7f455912966219a93494da1c0d
Hayk Hovhannisyan <hovahnnisayn@mail.ru>
2024-05-12 14:28:51 +0400

    Removed if/else statements,used factory pattern for parsing images and videos

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import os
 import argparse
 import validators
-from parsers.image_parser import ImgParser
-from parsers.video_parser import VideoParser
+from parsers.factory_initializer import initialize_factory
 
 
 def main():
@@ -10,43 +9,32 @@ def main():
     parser.add_argument('-url', '--url', type=str, help='URL of the webpage containing images/videos')
     parser.add_argument('-d', '--directory', type=str, help='Directory to save downloaded images/videos')
     parser.add_argument('-l', '--limit', type=int, help='Set Limit to downloading videos')
-    parser.add_argument('-pt', '--parse_type', type=str, choices=['image', 'video'], help='Type of content to parse ('
-                                                                                          'image or video)')
-
+    parser.add_argument('-pt', '--parse_type', type=str, choices=['images', 'videos'], help='Type of content to parse ('
+                                                                                            'images or videos)')
     args = parser.parse_args()
-    url = args.url
-    directory = args.directory
-    parse_type = args.parse_type
 
-    if url is None or directory is None:
+    parsed_args = {
+        'url': args.url,
+        'directory': args.directory,
+        'max_videos': args.limit
+    }
+
+    if args.url is None or args.directory is None:
         print("Error: Please provide both URL and directory arguments.")
         return
 
-    if not validators.url(url):
+    if not validators.url(args.url):
         print("Error: Invalid URL provided.")
         return
 
-    if not os.path.exists(directory):
-        os.makedirs(directory)
+    if not os.path.exists(args.directory):
+        os.makedirs(args.directory)
 
-    if parse_type == 'image':
-        img_parser = ImgParser(url=url, directory=directory)
-        img_parser.parse()
-        print("Images downloaded successfully.")
+    factory = initialize_factory()
 
-    elif parse_type == 'video':
-        if args.limit is None:
-            print("Error: Please provide the limit ")
-            os.removedirs(directory)
-            return
-        max_videos = args.limit
-        video_parser = VideoParser(url=url, directory=directory, max_videos=max_videos)
-        video_parser.parse()
-        print("Videos downloaded successfully.")
-    else:
-        print("Error: Please choose what you want to download: image/video")
-        os.removedirs(directory)
-        return
+    parser = factory.create(args.parse_type, **parsed_args)
+    parser.parse()
+    print(f"{args.parse_type.upper()} downloaded successfully")
 
 
 if __name__ == "__main__":
