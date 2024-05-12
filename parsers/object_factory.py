1b86ac34c657cafbaff63a21289679c4fffcf9bc
Hayk Hovhannisyan <hovahnnisayn@mail.ru>
2024-05-12 14:09:43 +0400

    Added ObjectFactory class in a new file named 'object_factory',this class helps to implement the factory design pattern

---
+++ parsers/object_factory.py
@@ -0,0 +1,17 @@
+
+
+class ObjectFactory:
+    def __init__(self):
+        self.builders = {}
+
+    def register_builder(self, key, builder):
+        self.builders[key] = builder
+
+    def create(self, key, **kwargs):
+        builder = self.builders.get(key)
+        if not builder:
+            raise ValueError(key)
+        return builder(**kwargs)
+
+
+
