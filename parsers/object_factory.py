cf8f52cc5abbb4287d03777a60b0b9ed25e3667b
Hayk2508 <145898819+Hayk2508@users.noreply.github.com>
2024-05-14 11:11:05 +0400

    Removed creation of an instance of ObjectFactory(FACTORY),removed decorator

--- parsers/object_factory.py
+++ parsers/object_factory.py
@@ -6,22 +6,12 @@ class ObjectFactory:
         self.builders[key] = builder
 
     def create(self, key, **kwargs):
-        builder = factory.builders.get(key)
+        builder = self.builders.get(key)
         if not builder:
             raise ValueError(f"No builder registered for key: {key}")
         return builder(**kwargs)
 
 
-def initialize_factory():
-    return ObjectFactory()
 
 
-factory = initialize_factory()
 
-
-def register_builder(key):
-    def decorator(cls):
-        factory.register_builder(key, cls())
-        return cls
-
-    return decorator
