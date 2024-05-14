abd263f35a5ede2baffc0aca3f08ab99ff37ea04
Hayk2508 <145898819+Hayk2508@users.noreply.github.com>
2024-05-14 11:05:54 +0400

    Added a new file named '__init__.py' in parsers folder to make it package,and added Factory object initialization with decorator into this file

---
+++ parsers/__init__.py
@@ -0,0 +1,10 @@
+from parsers.object_factory import ObjectFactory
+FACTORY = ObjectFactory()
+
+
+def register_builder(key):
+    def decorator(cls):
+        FACTORY.register_builder(key, cls())
+        return cls
+
+    return decorator
