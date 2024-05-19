a5d521683884b6a4128d86a9b82c87d0103d85b9
Hayk2508 <145898819+Hayk2508@users.noreply.github.com>
2024-05-19 16:49:13 +0400

    Added a new view function for an API

---
+++ myproject/parsers/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.http import require_GET
+
+from parsers import FACTORY
+from parsers.image_parser import ImgParserBuilder
+from parsers.video_parser import VideoParserBuilder
+
+
+@require_GET
+def parse_content(request):
+    url = request.GET.get("url")
+    parse_type = request.GET.get("parse-type")
+
+    if not url or not parse_type:
+        return JsonResponse(
+            {"error": "Both url and parse-type parameters are required"}, status=400
+        )
+
+    parsed_args = {
+        "url": url,
+        "directory": "",
+        "max_videos": 0,
+    }
+
+    parser = FACTORY.create(parse_type, **parsed_args)
+    parsed_objects = parser.parse(download_content=False)
+    parsed_objects_list = []
+    for obj in parsed_objects:
+        if parse_type in ["images", "videos"]:
+            parsed_objects_list.append({"obj-type": parse_type, "data": {"url": obj}})
+        else:
+            parsed_objects_list.append({"obj-type": parse_type, "data": {"text": obj}})
+
+    return JsonResponse(parsed_objects_list, safe=False)
