df56eedc2170ad88dde06e568db56c48fa9a79c4
Hayk2508 <145898819+Hayk2508@users.noreply.github.com>
2024-05-19 17:08:55 +0400

    Converted to Django Project,and did requirements from the issue

---
+++ myproject/parsers/management/commands/download_content.py
@@ -0,0 +1,62 @@
+import os
+import validators
+from django.core.management.base import BaseCommand
+
+from ... import FACTORY
+from ...image_parser import ImgParserBuilder
+from ...video_parser import VideoParserBuilder
+
+
+class Command(BaseCommand):
+    help = "Download contents from a webpage."
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "-url",
+            "--url",
+            type=str,
+            help="URL of the webpage containing images/videos",
+        )
+        parser.add_argument(
+            "-d",
+            "--directory",
+            type=str,
+            help="Directory to save downloaded images/videos",
+        )
+        parser.add_argument(
+            "-l", "--limit", type=int, help="Set Limit to downloading videos"
+        )
+        parser.add_argument(
+            "-pt",
+            "--parse_type",
+            type=str,
+            choices=["images", "videos"],
+            help="Type of content to parse (images or videos)",
+        )
+
+    def handle(self, *args, **options):
+        url = options["url"]
+        directory = options["directory"]
+        limit = options["limit"]
+        parse_type = options["parse_type"]
+
+        if url is None or directory is None:
+            self.stderr.write("Error: Please provide both URL and directory arguments.")
+            return
+
+        if not validators.url(url):
+            self.stderr.write("Error: Invalid URL provided.")
+            return
+
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+        parsed_args = {
+            "url": url,
+            "directory": directory,
+            "max_videos": limit,
+        }
+
+        parser = FACTORY.create(parse_type, **parsed_args)
+        parser.parse(download_content=True)
+        self.stdout.write(f"{parse_type.upper()} downloaded successfully")
---
+++ myproject/parsers/tests/test_media_parser.py
@@ -0,0 +1,50 @@
+import os
+import unittest
+from unittest.mock import patch
+
+from parsers.media_parser import MediaParser
+import tempfile
+
+
+class TestMediaParser(unittest.TestCase):
+    @patch.multiple("parsers.media_parser.MediaParser", __abstractmethods__=set())
+    def setUp(self) -> None:
+        self.parser = MediaParser(
+            url="https://example.com", directory="/path/to/directory"
+        )
+
+    @patch("parsers.media_parser.requests.get")
+    def test_fetch(self, mock_requests):
+        mock_requests.return_value.content = b"""
+            <html>
+                <body>
+                    <h1>Example HTML Content</h1>
+                    <p>This is paragraph 1.</p>
+                    <p>This is paragraph 2.</p>
+                    <p>This is paragraph 3.</p>
+                </body>
+            </html>
+        """
+
+        tag = "p"
+
+        result = self.parser.fetch(tag)
+        self.assertEqual(len(result), 3)
+
+    @patch("parsers.media_parser.requests.get")
+    def test_download(self, mock_requests):
+        mock_content = b"Image content"
+        mock_requests.return_value.content = mock_content
+        with tempfile.TemporaryDirectory() as tmp_dir_name:
+            media_url = "https://example.com/image.jpg"
+            media_name = "image.jpg"
+            self.parser.directory = tmp_dir_name
+            self.parser.download(media_url)
+
+            file_path = os.path.join(tmp_dir_name, media_name)
+            with open(file_path, "rb") as f:
+                self.assertEqual(f.read(), mock_content)
+
+
+if __name__ == "__main__":
+    unittest.main()
