a656e3eb993299db7f13a9467be2360da7fa9f3e
nikolay.durgaryan <nikolay.durgaryan@F4LC-69>
2024-05-21 15:57:43 +0400

    path fix

--- web-server/parsers/urls.py
+++ web-server/parsers/urls.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import parse_content
+from .views import parse_content, ParsersApiView
 
-urlpatterns = [path("parse/", parse_content, name="parse_content")]
+urlpatterns = [
+    path("parse", parse_content, name="parse_content"),
+    path("parse-content", ParsersApiView.as_view())
+]
+
