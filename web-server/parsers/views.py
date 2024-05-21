4b6c0756cf3ffe87bd97cc3b3d513620d9cba76d
nikolay.durgaryan <nikolay.durgaryan@F4LC-69>
2024-05-21 15:01:53 +0400

    add rest_framework

--- web-server/parsers/views.py
+++ web-server/parsers/views.py
@@ -1,9 +1,44 @@
 from django.http import JsonResponse
 from django.views.decorators.http import require_GET
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 from parsers.core import FACTORY
-from parsers.core.video_parser import VideoParserBuilder
-from parsers.core.image_parser import ImgParserBuilder
+
+
+class ParsersApiView(APIView):
+    def get(self, request):
+        url = request.GET.get("url")
+        parse_type = request.GET.get("parse-type")
+
+        if not url or not parse_type:
+            return Response(
+                {"error": "Both url and parse-type parameters are required"}, status=status.HTTP_400_BAD_REQUEST
+            )
+
+        parsed_args = {
+            "url": url,
+            "directory": "",
+            "max_videos": 0,
+        }
+
+        parser = FACTORY.create(parse_type, **parsed_args)
+        parsed_objects = parser.parse(download_content=False)
+        parsed_objects_list = []
+        for obj in parsed_objects:
+            obj_type = "url" if parse_type in ["images", "videos"] else "text"
+
+            parsed_objects_list.append(
+                {
+                    "obj-type": parse_type,
+                    "data": {
+                        obj_type: obj
+                    }
+                }
+            )
+
+        return Response(parsed_objects_list, status=status.HTTP_200_OK)
 
 
 @require_GET
