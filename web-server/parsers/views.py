38b113a98386b8566b6a1c148989e5985ec94979
Hayk2508 <145898819+Hayk2508@users.noreply.github.com>
2024-05-22 21:39:17 +0400

    Modified view function to use serializers

--- web-server/parsers/views.py
+++ web-server/parsers/views.py
@@ -1,20 +1,21 @@
-from django.http import JsonResponse
-from django.views.decorators.http import require_GET
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.exceptions import APIException
+from rest_framework.response import Response
 
 from parsers.core import FACTORY
 from parsers.core.video_parser import VideoParserBuilder
 from parsers.core.image_parser import ImgParserBuilder
+from parsers.serializers import ParseContentReqSerializer, ParseContentRespSerializer
 
 
-@require_GET
+@api_view(['GET'])
 def parse_content(request):
-    url = request.GET.get("url")
-    parse_type = request.GET.get("parse-type")
-
-    if not url or not parse_type:
-        return JsonResponse(
-            {"error": "Both url and parse-type parameters are required"}, status=400
-        )
+    serializer_req = ParseContentReqSerializer(data=request.query_params)
+    if not serializer_req.is_valid():
+        raise APIException(serializer_req.errors, code=status.HTTP_400_BAD_REQUEST)
+    url = serializer_req.validated_data["url"]
+    parse_type = serializer_req.validated_data["parse_type"]
 
     parsed_args = {
         "url": url,
@@ -23,12 +24,11 @@ def parse_content(request):
     }
 
     parser = FACTORY.create(parse_type, **parsed_args)
-    parsed_objects = parser.parse(download_content=False)
-    parsed_objects_list = []
-    for obj in parsed_objects:
-        if parse_type in ["images", "videos"]:
-            parsed_objects_list.append({"obj-type": parse_type, "data": {"url": obj}})
-        else:
-            parsed_objects_list.append({"obj-type": parse_type, "data": {"text": obj}})
+    parsed_objects = parser.parse()
+    response_data = [{"obj_type": parse_type, "data": obj} for obj in parsed_objects]
+    serializer_resp = ParseContentRespSerializer(data=response_data,  many=True)
 
-    return JsonResponse(parsed_objects_list, safe=False)
+    if not serializer_resp.is_valid():
+        raise APIException(serializer_resp.errors, code=status.HTTP_400_BAD_REQUEST)
+    return Response(serializer_resp.validated_data)
+
