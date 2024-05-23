8fa89d340c1f5c009fa53af1107bb963d4fd4488
Hayk2508 <145898819+Hayk2508@users.noreply.github.com>
2024-05-23 22:19:32 +0400

    Created a function that checks validity of data and return response

---
+++ web-server/parsers/core/services/response_service.py
@@ -0,0 +1,10 @@
+from rest_framework.response import Response
+
+
+def create_response(data, serializer_class) -> Response:
+    serializer_resp = serializer_class(data=data, many=True)
+    if not serializer_resp.is_valid():
+        raise ValueError("Internal Server Error")
+    return Response(serializer_resp.validated_data)
+
+
