ec3f7fc5835c28bc9e94b76829e8c9022e118f94
Hayk2508 <145898819+Hayk2508@users.noreply.github.com>
2024-05-23 22:20:04 +0400

    Used 'create_respone' function

--- web-server/parsers/views.py
+++ web-server/parsers/views.py
@@ -1,9 +1,9 @@
-from rest_framework import status
 from rest_framework.decorators import api_view
-from rest_framework.exceptions import APIException
 from rest_framework.response import Response
 
 from parsers.core import FACTORY
+from parsers.core.media_parser import MediaParser
+from parsers.core.services.response_service import create_response
 from parsers.core.video_parser import VideoParserBuilder
 from parsers.core.image_parser import ImgParserBuilder
 from parsers.serializers import ParseContentReqSerializer, ParseContentRespSerializer
@@ -12,8 +12,7 @@ from parsers.serializers import ParseContentReqSerializer, ParseContentRespSeria
 @api_view(["GET"])
 def parse_content(request):
     serializer_req = ParseContentReqSerializer(data=request.query_params)
-    if not serializer_req.is_valid():
-        raise APIException(serializer_req.errors, code=status.HTTP_400_BAD_REQUEST)
+    serializer_req.is_valid(raise_exception=True)
     url = serializer_req.validated_data["url"]
     parse_type = serializer_req.validated_data["parse_type"]
 
@@ -25,9 +24,5 @@ def parse_content(request):
 
     parser = FACTORY.create(parse_type, **parsed_args)
     parsed_objects = parser.parse()
-    response_data = [{"obj_type": parse_type, "data": obj} for obj in parsed_objects]
-    serializer_resp = ParseContentRespSerializer(data=response_data, many=True)
-
-    if not serializer_resp.is_valid():
-        raise APIException(serializer_resp.errors, code=status.HTTP_400_BAD_REQUEST)
-    return Response(serializer_resp.validated_data)
+    response_data = [{"obj_type": parse_type, "data": MediaParser.to_data(obj)} for obj in parsed_objects]
+    return create_response(data=response_data, serializer_class=ParseContentRespSerializer)
