4e6189786f37c418a4c2af1f3d8513acf2410ddd
nikolay.durgaryan <nikolay.durgaryan@F4LC-69>
2024-05-30 14:06:06 +0400

    add Logging middleware

--- web-server/core/settings.py
+++ web-server/core/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "core.middleware.LogMiddleware.LogMiddleware",
 ]
 
 ROOT_URLCONF = "core.urls"
--- web-server/parsers/urls.py
+++ web-server/parsers/urls.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import parse_content
+from .views import parse_content, ppppp, ddddd
 
-urlpatterns = [path("parse/", parse_content, name="parse_content")]
+urlpatterns = [
+    path("parse/", parse_content, name="parse_content"),
+    path("pp", ppppp, name="pp"),
+    path("dd", ddddd, name="dd"),
+]
