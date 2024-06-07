308a036cbd8ec831e92a77517c3611a8ae34d1ce
Hayk2508 <145898819+Hayk2508@users.noreply.github.com>
2024-06-07 22:23:05 +0400

    Created new models

--- web-server/parsers/admin.py
+++ web-server/parsers/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Url, VideoParsedObject, ImageParser, VideoParser, ImageParsedObject
 
 # Register your models here.
+admin.site.register(Url)
+
+admin.site.register(VideoParsedObject)
+admin.site.register(ImageParsedObject)
+admin.site.register(ImageParser)
+admin.site.register(VideoParser)
--- web-server/parsers/models.py
+++ web-server/parsers/models.py
@@ -1,3 +1,59 @@
+from django.contrib.contenttypes.fields import GenericRelation, GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
-# Create your models here.
+
+class Date(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Url(Date):
+    url = models.URLField(unique=True)
+
+
+class Parser(Date):
+    url = models.ForeignKey(Url, on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey("content_type", "object_id")
+
+    class Meta:
+        abstract = True
+
+
+class ImageParser(Parser):
+    pass
+
+
+class VideoParser(Parser):
+    pass
+
+
+class ParsedObject(Date):
+
+    class Meta:
+        abstract = True
+
+
+class ImageParsedObject(ParsedObject):
+    parsed_objects = GenericRelation(
+        ImageParser,
+        content_type_field="content_type",
+        object_id_field="object_id",
+        related_query_name="parser",
+    )
+    image_url = models.URLField()
+
+
+class VideoParsedObject(ParsedObject):
+    parsed_objects = GenericRelation(
+        VideoParser,
+        content_type_field="content_type",
+        object_id_field="object_id",
+        related_query_name="parser",
+    )
+    video_url = models.URLField()
