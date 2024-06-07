aaa33411ff052440e84a9703b6ae631e2d0692c9
Hayk2508 <145898819+Hayk2508@users.noreply.github.com>
2024-06-07 22:23:51 +0400

    Modified

--- web-server/parsers/views.py
+++ web-server/parsers/views.py
@@ -1,6 +1,11 @@
+from django.contrib.contenttypes.models import ContentType
 from rest_framework.decorators import api_view
 from parsers.core import FACTORY
+
 from core.services.response_service import create_response
+from parsers.core.image_parser import ImgParserBuilder
+from parsers.core.video_parser import VideoParserBuilder
+from parsers.models import Url, ImageParser, VideoParser, ParsedObject
 from parsers.serializers import ParseContentReqSerializer, ParseContentRespSerializer
 
 
@@ -8,20 +13,21 @@ from parsers.serializers import ParseContentReqSerializer, ParseContentRespSeria
 def parse_content(request):
     serializer_req = ParseContentReqSerializer(data=request.query_params)
     serializer_req.is_valid(raise_exception=True)
+
     url = serializer_req.validated_data["url"]
     parse_type = serializer_req.validated_data["parse_type"]
-
-    parsed_args = {
-        "url": url,
-        "directory": "",
-        "max_videos": 0,
-    }
+    parsed_args = {"url": url}
 
     parser = FACTORY.create(parse_type, **parsed_args)
     parsed_objects = parser.parse()
-    response_data = [
-        {"obj_type": parse_type, "data": parser.to_data(obj)} for obj in parsed_objects
-    ]
+
+    response_data = []
+
+    for parsed_object in parsed_objects:
+        data = parser.to_data(parsed_object)
+        content_type = ContentType.objects.get_for_model(parsed_object)
+        response_data.append({"obj_type": content_type.name, "data": data})
+
     return create_response(
         data=response_data, serializer_class=ParseContentRespSerializer, many=True
     )
