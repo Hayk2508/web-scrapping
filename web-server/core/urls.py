024895e4e4ea19b94ff1fac2220875433c3169c2
Hayk2508 <145898819+Hayk2508@users.noreply.github.com>
2024-06-08 21:39:21 +0400

    Added logic to automatically manage url-routing(for CRUD)

--- web-server/core/urls.py
+++ web-server/core/urls.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from parsers.views import ImageParsedObjectViewSet, VideoParsedObjectViewSet
+
+router = DefaultRouter()
+router.register(r"image-parsed-objects", ImageParsedObjectViewSet)
+router.register(r"video-parsed-objects", VideoParsedObjectViewSet)
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/parsers/", include("parsers.urls")),
+    path("api/", include(router.urls)),
 ]
