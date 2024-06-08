c09ea14d7c7ab87bdf0cb05e8e1776fa2c18a3da
Hayk2508 <145898819+Hayk2508@users.noreply.github.com>
2024-06-08 21:43:00 +0400

    Removed 'migrations' from '.gitignore'

---
+++ web-server/parsers/migrations/0005_auto_20240607_1221.py
@@ -0,0 +1,34 @@
+from django.db import migrations
+from django.contrib.contenttypes.models import ContentType
+
+
+def populate_content_type_and_object_id(apps, schema_editor):
+    ImageParser = apps.get_model("parsers", "ImageParser")
+    VideoParser = apps.get_model("parsers", "VideoParser")
+
+    image_content_type = ContentType.objects.get_for_model(ImageParser)
+    video_content_type = ContentType.objects.get_for_model(VideoParser)
+
+    for image_parser in ImageParser.objects.all():
+        image_parser.content_type = image_content_type
+        image_parser.object_id = image_parser.id
+        image_parser.save()
+
+    for video_parser in VideoParser.objects.all():
+        video_parser.content_type = video_content_type
+        video_parser.object_id = video_parser.id
+        video_parser.save()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        (
+            "parsers",
+            "0004_auto_20240607_1215",
+        ),  # Replace with the actual previous migration name
+    ]
+
+    operations = [
+        migrations.RunPython(populate_content_type_and_object_id),
+    ]
