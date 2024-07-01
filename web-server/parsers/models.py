2012153f1c860d5d330363ceeca7ab2798ec6955
Hayk2508 <145898819+Hayk2508@users.noreply.github.com>
2024-07-01 16:03:41 +0400

    Created builder for both 'ImageParsedObject' and 'VideoParsedObject'

--- web-server/parsers/models.py
+++ web-server/parsers/models.py
@@ -1,14 +1,14 @@
 from django.db import models
-from polymorphic.managers import PolymorphicManager
 
 from core.base_models import TimeStamp
 from polymorphic.models import PolymorphicModel
 
-from parsers.core.media_parser import Parsers
+from parsers.core import register_parsed_objects_builder
+from parsers.core.enums import Parsers
 
 
 class Parser(TimeStamp, PolymorphicModel):
-    url = models.URLField(unique=True)
+    url = models.URLField()
 
 
 class ImageParser(Parser):
@@ -22,6 +22,18 @@ class VideoParser(Parser):
 class ParsedObject(TimeStamp, PolymorphicModel):
     parser = models.ForeignKey(Parser, on_delete=models.CASCADE)
 
+    def to_type(self):
+        pass
+
+    def to_data(self):
+        pass
+
+
+@register_parsed_objects_builder(Parsers.IMAGES)
+class ImageParsedObjectBuilder:
+    def __call__(self, image_url: str, parser_id, **_ignored):
+        return ImageParsedObject(image_url=image_url, parser_id=int(parser_id))
+
 
 class ImageParsedObject(ParsedObject):
     image_url = models.URLField()
@@ -31,6 +43,12 @@ class ImageParsedObject(ParsedObject):
 
     def to_data(self):
         return {"image_url": self.image_url}
+
+
+@register_parsed_objects_builder(Parsers.VIDEOS)
+class VideoParsedObjectBuilder:
+    def __call__(self, video_url: str, parser_id, **_ignored):
+        return VideoParsedObject(video_url=video_url, parser_id=int(parser_id))
 
 
 class VideoParsedObject(ParsedObject):
