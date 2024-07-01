0ac20f5ddb5b92b420b8c356bb70211ec676bdfd
Hayk2508 <145898819+Hayk2508@users.noreply.github.com>
2024-07-01 16:00:49 +0400

    Added optimization to both image_parser and video_parser,added creation of objects according to max_videos

--- web-server/parsers/core/image_parser.py
+++ web-server/parsers/core/image_parser.py
@@ -1,6 +1,8 @@
-from . import register_builder
-from .media_parser import MediaParser, Parsers
-from ..models import ImageParser, ImageParsedObject, Parser, ParsedObject
+from . import register_parsers_builder
+from .media_parser import MediaParser
+from .enums import Parsers
+from ..models import ImageParser, ImageParsedObject
+from django.db import transaction
 
 
 class ImgParser(MediaParser):
@@ -8,11 +10,12 @@ class ImgParser(MediaParser):
         super().__init__(url=url)
 
     def parse(self):
-        image_parser, created = ImageParser.objects.get_or_create(url=self.url)
+        image_parser, _ = ImageParser.objects.get_or_create(url=self.url)
+        image_parsed_objects = set()
+        image_urls = []
         img_tags = self.fetch(tag="img")
         for img in img_tags:
             img_url = img.get("src")
-
             if not img_url:
                 potential_attributes = ["data-src", "srcset"]
                 for attr in potential_attributes:
@@ -23,16 +26,17 @@ class ImgParser(MediaParser):
                         break
             if not img_url.startswith(("http://", "https://")):
                 img_url = self.process_url(media_url=img_url)
+            image_urls.append(img_url)
+        with transaction.atomic():
+            for image_url in image_urls:
+                image_parsed_object, _ = ImageParsedObject.objects.get_or_create(
+                    image_url=image_url, parser=image_parser
+                )
+                image_parsed_objects.add(image_parsed_object)
+        return image_parsed_objects
 
-            if not ImageParsedObject.objects.filter(
-                image_url=img_url, parser=image_parser
-            ).exists():
-                ImageParsedObject.objects.create(image_url=img_url, parser=image_parser)
 
-        return ParsedObject.objects.filter(parser=image_parser)
-
-
-@register_builder(Parsers.IMAGES)
+@register_parsers_builder(Parsers.IMAGES)
 class ImgParserBuilder:
     def __call__(self, url: str, **_ignored):
         return ImgParser(url=url)
--- web-server/parsers/core/video_parser.py
+++ web-server/parsers/core/video_parser.py
@@ -1,6 +1,8 @@
-from .media_parser import MediaParser, Parsers
-from . import register_builder
-from ..models import VideoParsedObject, VideoParser as Video_Parser, ParsedObject
+from django.db import transaction
+from .media_parser import MediaParser
+from .enums import Parsers
+from . import register_parsers_builder
+from ..models import VideoParsedObject, VideoParser as Video_Parser
 
 
 class VideoParser(MediaParser):
@@ -10,26 +12,32 @@ class VideoParser(MediaParser):
         self.max_videos = max_videos
 
     def parse(self):
-        video_parser, created = Video_Parser.objects.get_or_create(url=self.url)
-
-        video_tags = self.fetch(tag="video")
-
+        video_parser, _ = Video_Parser.objects.get_or_create(url=self.url)
+        video_parsed_objects = []
+        video_tags = self.fetch(tag="video")[: self.max_videos]
+        video_urls = []
         for video in video_tags:
-            video_url = video.find("a")["href"]
+            video_url = video.find("a")
+            if video_url:
+                video_url = video_url.get("href")
+            else:
+                video_url = video.get("src")
+
             if not video_url.startswith(("http://", "https://")):
                 video_url = self.process_url(media_url=video_url)
 
-            if not VideoParsedObject.objects.filter(
-                video_url=video_url, parser=video_parser
-            ).exists():
-                VideoParsedObject.objects.create(
+            video_urls.append(video_url)
+        with transaction.atomic():
+            for video_url in video_urls:
+                video_parsed_object, _ = VideoParsedObject.objects.get_or_create(
                     video_url=video_url, parser=video_parser
                 )
+                video_parsed_objects.append(video_parsed_object)
 
-        return ParsedObject.objects.filter(parser=video_parser)
+        return video_parsed_objects
 
 
-@register_builder(Parsers.VIDEOS)
+@register_parsers_builder(Parsers.VIDEOS)
 class VideoParserBuilder:
     def __call__(self, url: str, max_videos: int):
         return VideoParser(url=url, max_videos=max_videos)
