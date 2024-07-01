e719b5d878d78f54b0bc4b983e2922b4ef5d0e63
Hayk2508 <145898819+Hayk2508@users.noreply.github.com>
2024-07-01 15:55:49 +0400

    Added function to help with response

--- web-server/core/services/response_service.py
+++ web-server/core/services/response_service.py
@@ -1,8 +1,24 @@
 from rest_framework.response import Response
 
 
-def create_response(data, serializer_class, many=False) -> Response:
-    serializer_resp = serializer_class(data=data, many=many)
-    if not serializer_resp.is_valid():
-        raise ValueError("Internal Server Error")
-    return Response(serializer_resp.validated_data)
+
+
+def validate_response(response_serializer, data=None, instance=None, many=False):
+    if instance is not None:
+        serializer = response_serializer(instance, many=many)
+        return Response(serializer.data)
+    serializer = response_serializer(data=data)
+    serializer.is_valid(raise_exception=True)
+
+
+def create_response(data=None, instance=None, serializer=None, many=False) -> Response:
+    if serializer is None:
+        return Response(data)
+    resp = validate_response(
+        response_serializer=serializer, data=data, instance=instance, many=many
+    )
+
+    if instance is not None:
+        assert resp is not None
+        return resp
+    return Response(data)
