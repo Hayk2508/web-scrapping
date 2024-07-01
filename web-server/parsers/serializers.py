1da913d65a788d15d612130e1b605b33b856a53d
Hayk2508 <145898819+Hayk2508@users.noreply.github.com>
2024-07-01 16:05:18 +0400

    Created one more serializer for creating objects with POST

--- web-server/parsers/serializers.py
+++ web-server/parsers/serializers.py
@@ -1,17 +1,13 @@
-from parsers.core import FACTORY
+from parsers.core import PARSERS_FACTORY, PARSED_OBJECTS_FACTORY
 from rest_framework import serializers
 
-from parsers.models import ParsedObject, ImageParsedObject, VideoParsedObject, Parser
+from parsers.models import ParsedObject, ImageParsedObject, VideoParsedObject
 
 
 class ParseContentReqSerializer(serializers.Serializer):
     url = serializers.URLField()
-    parse_type = serializers.ChoiceField(choices=list(FACTORY.builders.keys()))
-
-
-class ParseContentRespSerializer(serializers.Serializer):
-    obj_type = serializers.CharField()
-    data = serializers.DictField()
+    parse_type = serializers.ChoiceField(choices=list(PARSERS_FACTORY.builders.keys()))
+    max_videos = serializers.IntegerField(default=None)
 
 
 class ParsedObjectSerializer(serializers.ModelSerializer):
@@ -20,10 +16,19 @@ class ParsedObjectSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = ParsedObject
-        fields = ("id", "created_at", "updated_at", "obj_type", "data")
+        fields = ("id", "created_at", "updated_at", "obj_type", "data", "parser_id")
 
     def get_obj_type(self, obj):
         return obj.to_type()
 
     def get_data(self, obj):
         return obj.to_data()
+
+
+class CreateParsedObjectSerializer(serializers.Serializer):
+    obj_type = serializers.ChoiceField(
+        choices=list(PARSED_OBJECTS_FACTORY.builders.keys())
+    )
+    image_url = serializers.CharField(required=False)
+    video_url = serializers.CharField(required=False)
+    parser_id = serializers.IntegerField()
