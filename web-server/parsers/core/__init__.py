3e7f0077d65d375b1fd71510e4adfcab18730a81
Hayk2508 <145898819+Hayk2508@users.noreply.github.com>
2024-07-01 15:57:32 +0400

    Created objects of 'ObjectFactory' and 2 decorators

--- web-server/parsers/core/__init__.py
+++ web-server/parsers/core/__init__.py
@@ -1,11 +1,20 @@
 from parsers.core.object_factory import ObjectFactory
 
-FACTORY = ObjectFactory()
+PARSERS_FACTORY = ObjectFactory()
+PARSED_OBJECTS_FACTORY = ObjectFactory()
 
 
-def register_builder(key):
+def register_parsers_builder(key):
     def decorator(cls):
-        FACTORY.register_builder(key.value, cls())
+        PARSERS_FACTORY.register_builder(key.value, cls())
+        return cls
+
+    return decorator
+
+
+def register_parsed_objects_builder(key):
+    def decorator(cls):
+        PARSED_OBJECTS_FACTORY.register_builder(key.value, cls())
         return cls
 
     return decorator
