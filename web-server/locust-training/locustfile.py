efa976f1a364f27894ff81aab76872380ad7e85d
Hayk2508 <145898819+Hayk2508@users.noreply.github.com>
2024-07-22 17:38:03 +0400

    Added a new file for load testing

---
+++ web-server/locust-training/locustfile.py
@@ -0,0 +1,9 @@
+from locust import HttpUser, task, between
+
+
+class LoadTestUser(HttpUser):
+    wait_time = between(1, 5)
+
+    @task
+    def load_test_endpoint(self):
+        self.client.get('/api/parsed-objects')
