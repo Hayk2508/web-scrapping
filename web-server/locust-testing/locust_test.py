678039f149df7bdbe698c62696250083841b1423
Hayk2508 <145898819+Hayk2508@users.noreply.github.com>
2024-07-22 17:15:48 +0400

    Attempt to fix test error

--- web-server/locust-testing/locust_test.py
+++
@@ -1,9 +0,0 @@
-from locust import HttpUser, task, between
-
-
-class LoadTestUser(HttpUser):
-    wait_time = between(1, 5)
-
-    @task
-    def load_test_endpoint(self):
-        self.client.get("/api/parsed-objects/")
