603ccbb52a4d9ae7cbb2d5e3c03b948b05c3b723
Hayk2508 <145898819+Hayk2508@users.noreply.github.com>
2024-07-24 17:53:14 +0400

    Changed endpoint and deleted 'wait_time'

--- load-testing/locustfile.py
+++ load-testing/locustfile.py
@@ -2,8 +2,9 @@ from locust import HttpUser, task, between
 
 
 class LoadTestUser(HttpUser):
-    wait_time = between(1, 5)
 
     @task
     def load_test_endpoint(self):
-        self.client.get("/api/parsed-objects")
+        self.client.get(
+            "/api/parsers/parse/?url=https://preply.com/en/tutor-signup&parse_type=images"
+        )
