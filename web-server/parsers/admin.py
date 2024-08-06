46eef1bc5b20ca1581dd9369280fc07a81fc70bd
Hayk2508 <145898819+Hayk2508@users.noreply.github.com>
2024-08-06 13:14:57 +0400

    Registered ImageParsedObject and VideoParsedObject models with the Django admin site

--- web-server/parsers/admin.py
+++ web-server/parsers/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from parsers.models import VideoParsedObject, ImageParsedObject
+
+admin.site.register(ImageParsedObject)
+admin.site.register(VideoParsedObject)
