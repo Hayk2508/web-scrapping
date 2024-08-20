e5a064452e05cdea1c58ed2578469e4e5b81d6f4
Hayk2508 <145898819+Hayk2508@users.noreply.github.com>
2024-08-20 17:36:26 +0400

    temp: add some checks

--- web-server/user_auth/views.py
+++ web-server/user_auth/views.py
@@ -8,7 +8,8 @@ from rest_framework.response import Response
 def verify_user_credentials(request):
     username = request.data.get("username")
     password = request.data.get("password")
-
+    if not username or not password:
+        return Response({"detail": "Username and password are required."}, status=status.HTTP_400_BAD_REQUEST)
     user = authenticate(username=username, password=password)
     if user is not None:
         return Response(status=200)
