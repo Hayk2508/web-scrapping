4f7bcb61a1b86ba5b48e16d7612512d964622304
Hayk2508 <145898819+Hayk2508@users.noreply.github.com>
2024-08-20 17:38:41 +0400

    format

--- web-server/user_auth/views.py
+++ web-server/user_auth/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import authenticate
 from django.shortcuts import render
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -9,7 +10,10 @@ def verify_user_credentials(request):
     username = request.data.get("username")
     password = request.data.get("password")
     if not username or not password:
-        return Response({"detail": "Username and password are required."}, status=status.HTTP_400_BAD_REQUEST)
+        return Response(
+            {"detail": "Username and password are required."},
+            status=status.HTTP_400_BAD_REQUEST,
+        )
     user = authenticate(username=username, password=password)
     if user is not None:
         return Response(status=200)
