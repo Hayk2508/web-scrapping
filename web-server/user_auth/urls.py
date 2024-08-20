c156164ea970154947af80decc5f69d526dc3b79
Hayk2508 <145898819+Hayk2508@users.noreply.github.com>
2024-08-20 16:52:17 +0400

    Create endpoint to check if incoming data is valid(using default django User model)

--- web-server/user_auth/urls.py
+++ web-server/user_auth/urls.py
@@ -4,7 +4,8 @@ from rest_framework_simplejwt.views import (
 )
 from django.urls import path
 
+from user_auth.views import verify_user_credentials
+
 urlpatterns = [
-    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("check/", verify_user_credentials, name="verify_user_credentials"),
 ]
--- web-server/user_auth/views.py
+++ web-server/user_auth/views.py
@@ -1,3 +1,11 @@
+from django.contrib.auth import authenticate
 from django.shortcuts import render
+from rest_framework.decorators import api_view
 
-# Create your views here.
+
+@api_view(["POST"])
+def verify_user_credentials(request):
+    username = request.data.get("username")
+    password = request.data.get("password")
+
+    return authenticate(username=username, password=password) is not None
