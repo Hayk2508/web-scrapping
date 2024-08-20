235569e16aa812316c8de2bbc208d0da1703ab8f
Hayk2508 <145898819+Hayk2508@users.noreply.github.com>
2024-08-20 18:01:52 +0400

    Add decorator to let this endpoint be publicly accessible(no need auth)

--- web-server/user_auth/views.py
+++ web-server/user_auth/views.py
@@ -1,11 +1,13 @@
 from django.contrib.auth import authenticate
 from django.shortcuts import render
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
 
 @api_view(["POST"])
+@permission_classes([AllowAny])
 def verify_user_credentials(request):
     username = request.data.get("username")
     password = request.data.get("password")
